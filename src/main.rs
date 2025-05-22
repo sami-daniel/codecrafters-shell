11b65843cb2cc44018c1fd25cccd19de33ff0de6
Sami Daniel <samidanielpersonal@gmail.com>
2025-05-22 08:30:33 -0300

    --

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,17 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-
-    println!("{}: command not found", &input.trim())
+        match input {
+            _ => {    
+                println!("{}: command not found", &input.trim())
+            } 
+        }
+    }
 }
