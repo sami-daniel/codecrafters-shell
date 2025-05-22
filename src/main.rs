0426432a4ee8a99f0a4e335f2b78e3dfd3421c01
Sami Daniel <samidanielpersonal@gmail.com>
2025-05-22 08:43:25 -0300

    --

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::ExitCode;
 
-fn main() {
+fn main() -> ExitCode {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
-        
+
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-
-        match input {
-            _ => {    
-                println!("{}: command not found", &input.trim())
-            } 
+        let command: Vec<&str> = input.trim().split(' ').collect();
+        if let Some(command) = command.get(0) {
+            match *command {
+                "exit" => {
+                    let parts = input.chars();
+                    let exit_code = parts
+                        .last()
+                        .unwrap_or('0')
+                        .to_string()
+                        .parse::<u8>()
+                        .unwrap_or(0);
+                    return ExitCode::from(exit_code);
+                }
+                _ => {
+                    println!("{}: command not found", &input.trim())
+                }
+            }
+        } else {
+            return ExitCode::from(0);
         }
     }
 }
